253c266d50ca387dc744439dc9461a632fd056e1
bereketassefa <bereket.a.gedefaw@gmail.com>
2025-10-07 09:22:29 +0300

    Feat: making punishment configurable

--- src/components/ProductForm/ContributionTabForm.js
+++ src/components/ProductForm/ContributionTabForm.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React, { useRef } from "react";
+import { Grid , Box , Button} from "@material-ui/core";
 import { withTheme, withStyles } from "@material-ui/styles";
-import { combine, useTranslations, NumberInput, useModulesManager } from "@openimis/fe-core";
+import { combine, useTranslations, NumberInput, useModulesManager, TextInput } from "@openimis/fe-core";
 import Tooltip from '@material-ui/core/Tooltip';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
 import InfoIcon from '@material-ui/icons/Info';
+
 
 const ContributionTabForm = (props) => {
   const { edited, onEditedChanged, readOnly, classes } = props;
   const modulesManager = useModulesManager();
   const { formatMessage } = useTranslations("product.ContributionTabForm", modulesManager);
+  const conf = modulesManager.getConf("fe-product", "penalityConfig", [])
+  const inputRef = useRef(null);
+
+  const insertAtCursor = (text) => {
+    text = '{'+text+'}';
+    const input = inputRef.current;
+    console.log("input", input , input.selectionEnd , input.selectionStart);
+    if (!input) return; // safety check
+    const start = input.selectionStart;
+    const end = input.selectionEnd;
+    const value = edited?.formula ?? "";
+    const newValue =
+      value.substring(0, start) + text + value.substring(end);
+
+    onEditedChanged({...edited , formula: newValue});
+
+    // after state update, restore cursor just after inserted text
+    setTimeout(() => {
+      input.focus();
+      input.setSelectionRange(start + text.length, start + text.length);
+    }, 0);
+  };
+
+  function isValidMathExpression(expr) {
+    const regex = /^\s*(?:\{[a-zA-Z_][a-zA-Z0-9_]*\}|\d*\.?\d+|\((?:[^()]+|\([^()]*\))*\))\s*(?:[+\-*/]\s*(?:\{[a-zA-Z_][a-zA-Z0-9_]*\}|\d*\.?\d+|\((?:[^()]+|\([^()]*\))*\))\s*)*$/;
+    if(!regex.test(expr)){
+      return "Invalid Expression"
+    }
+  }
+
   return (
     <Grid container>
       <Grid item xs={3} className={classes.item}>
+        {/* {JSON.stringify(conf)} */}
         <Grid container justifyContent="center"
           alignItems="center" spacing={1}>
           <Grid item xs={1}>
@@ -75,16 +109,46 @@ const ContributionTabForm = (props) => {
           onChange={(penaltyPrice) => onEditedChanged({ ...edited, penaltyPrice })}
         />
       </Grid>
-      {/* <Grid item xs={3} className={classes.item}>
-        <NumberInput
-          module="product"
-          min={0}
-          label="generalAssemblyFee"
-          readOnly={readOnly}
-          value={edited?.generalAssemblyFee ?? ""}
-          onChange={(generalAssemblyFee) => onEditedChanged({ ...edited, generalAssemblyFee })}
-        />
-      </Grid> */}
+      <Grid item xs={12} className={classes.item} >
+        <Grid container justifyContent="center" spacing={3}>
+          <Grid item xs={6}>
+            <TextInput
+              module="product"
+              required
+              error={isValidMathExpression(edited?.penalityFormula ?? "")}
+              label="Panishment Formula"
+              readOnly={readOnly}
+              value={edited?.formula ?? ""}
+              inputRef={inputRef}
+              onChange={(formula) => onEditedChanged({ ...edited, penalityFormula })}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container>
+                {
+                conf.length > 0 ? conf.map((c, i) => (
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    onClick={() => insertAtCursor(c?.code)}
+                    style={{ textTransform: "none", padding: "12px 20px", width: '100%', margin: 2 }}
+                  >
+                    <Box display="flex" flexDirection="column" alignItems="flex-start" width="100%">
+                      <Typography variant="body1" style={{ fontWeight: "bold" }}>
+                        {c?.name}
+                      </Typography>
+                      <Typography variant="caption" color="inherit">
+                        {c?.description}
+                      </Typography>
+                    </Box>
+                  </Button>
+                )) : <div style={{ fontSize: 12 }}></div>
+              }
+            
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
